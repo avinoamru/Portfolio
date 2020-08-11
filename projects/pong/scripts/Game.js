a18cd6c1ada96d1fe35208bbed0902ede4875eb9
Avinoam Ruach <ruachavinoam@gmail.com>
2020-08-11 13:34:28 +0300

    updating files and adding some code

---
+++ projects/pong/scripts/Game.js
@@ -0,0 +1,52 @@
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var canvasContainer = document.querySelector(".canvas-container");
+canvasContainer.appendChild(canvas);
+var winWidth = window.innerWidth * 0.95;
+var winHeight = window.innerHeight * 0.95;
+var Player = /** @class */ (function () {
+    function Player(x, y) {
+        this.color = "rgb(255,255,255)";
+        this.width = 10;
+        this.height = winHeight * 0.1;
+        this.positionX = x;
+        this.positionY = y;
+    }
+    return Player;
+}());
+var Ball = {
+    x: winWidth / 2,
+    y: winHeight / 2,
+    radius: null
+};
+var player1 = new Player(50, winHeight / 2);
+var player2 = new Player(winWidth - 50, winHeight / 2);
+Ball.radius = player1.height / 8;
+window.addEventListener("keydown", function (e) {
+    switch (e.key) {
+        case "ArrowUp":
+            player1.positionY -= winHeight / 100;
+            break;
+        case "ArrowDown":
+            player1.positionY += winHeight / 100;
+            break;
+        default:
+            console.log("Invalid key: " + e.key);
+    }
+});
+var Game = function () {
+    winWidth = window.innerWidth * 0.95;
+    winHeight = window.innerHeight * 0.95;
+    canvas.width = winWidth;
+    canvas.height = winHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(player1.positionX, player1.positionY, player1.width, player1.height);
+    ctx.fillRect(player2.positionX, player2.positionY, player2.width, player2.height);
+    ctx.arc(Ball.x, Ball.y, Ball.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+    requestAnimationFrame(Game);
+};
+Game();
---
+++ projects/pong/scripts/Game.ts
@@ -0,0 +1,78 @@
+const canvas:HTMLCanvasElement = document.createElement("canvas");
+const ctx:CanvasRenderingContext2D = canvas.getContext("2d")
+const canvasContainer:HTMLElement = document.querySelector(".canvas-container")
+canvasContainer.appendChild(canvas)
+
+let winWidth:number = window.innerWidth * 0.95;
+let winHeight:number = window.innerHeight * 0.95;
+
+
+class Player{
+    positionX:number;
+    positionY:number;
+    color:string = "rgb(255,255,255)";
+    width:number = 10;
+    height:number = winHeight * 0.1
+constructor(x:number,y:number){
+    this.positionX = x;
+    this.positionY = y;
+}
+
+}
+
+const Ball:{
+    x:number,
+    y:number,
+    radius:number,
+    } = {
+        x:winWidth/2,
+        y:winHeight/2,
+        radius:null,
+}
+
+const player1 = new Player(50,winHeight/2)
+const player2 = new Player(winWidth-50,winHeight/2)
+Ball.radius = player1.height/8 
+
+
+window.addEventListener("keydown",(e)=>{
+    switch(e.key){
+        case "ArrowUp": player1.positionY -= winHeight/100
+        break;
+
+        case "ArrowDown": player1.positionY +=winHeight/100
+        break;
+        
+        default: 
+        console.log(`Invalid key: ${e.key}`)
+    }
+    
+    
+})
+
+const Game = (): void => {
+     winWidth= window.innerWidth * 0.95;
+     winHeight = window.innerHeight * 0.95;
+    
+   
+    canvas.width = winWidth 
+    canvas.height = winHeight 
+
+    
+
+  
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = "black";
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = "white"; 
+        ctx.fillRect(player1.positionX, player1.positionY,player1.width,player1.height)
+        ctx.fillRect(player2.positionX, player2.positionY,player2.width,player2.height)
+        ctx.arc(Ball.x,Ball.y,Ball.radius,0,Math.PI*2,false)
+        ctx.fill();
+
+        
+
+requestAnimationFrame(Game)
+}
+
+Game()
